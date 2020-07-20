d595286ab712dbc87feb0a73e93a2d42b619ad4f
Benjamin Luke Smith-Gordon <ben.smith.gordon@gmail.com>
2020-07-20 11:32:32 +0100

    Refactored code by destructuring
    Also added a ref to AddPlayerForm, and added PropTypes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import Header from './Header';
-import Player from './Player';
-import AddPlayerForm from './AddPlayerForm';
+import React, { Component } from "react";
+import Header from "./Header";
+import Player from "./Player";
+import AddPlayerForm from "./AddPlayerForm";
 
 class App extends Component {
   state = {
@@ -9,78 +9,75 @@ class App extends Component {
       {
         name: "Guil",
         score: 0,
-        id: 1
+        id: 1,
       },
       {
         name: "Treasure",
         score: 0,
-        id: 2
+        id: 2,
       },
       {
         name: "Ashley",
         score: 0,
-        id: 3
+        id: 3,
       },
       {
         name: "James",
         score: 0,
-        id: 4
-      }
-    ]
+        id: 4,
+      },
+    ],
   };
 
   prevPlayerId = 4;
 
   handleScoreChange = (index, value) => {
-    this.setState( prevState => ({
-      score: prevState.players[index].score += value
+    this.setState((prevState) => ({
+      score: (prevState.players[index].score += value),
     }));
-  }
+  };
 
   handleAddPlayer = (name) => {
-    this.setState( prevState =>  {
+    this.setState((prevState) => {
       return {
         players: [
           ...prevState.players,
           {
             name,
             score: 0,
-            id: this.prevPlayerId += 1
-          }
-        ]
+            id: (this.prevPlayerId += 1),
+          },
+        ],
       };
     });
-  }
+  };
 
   handleRemovePlayer = (id) => {
-    this.setState( prevState => {
+    this.setState((prevState) => {
       return {
-        players: prevState.players.filter(p => p.id !== id)
+        players: prevState.players.filter((p) => p.id !== id),
       };
     });
-  }
+  };
 
   render() {
     return (
       <div className="scoreboard">
-        <Header 
-          title="Scoreboard" 
-          players={this.state.players}
-        />
-  
+        <Header title="Scoreboard" players={this.state.players} />
+
         {/* Players list */}
-        {this.state.players.map( (player, index) =>
-          <Player 
+        {this.state.players.map((player, index) => (
+          <Player
             name={player.name}
             score={player.score}
             id={player.id}
             key={player.id.toString()}
             index={index}
             changeScore={this.handleScoreChange}
-            removePlayer={this.handleRemovePlayer}           
+            removePlayer={this.handleRemovePlayer}
           />
-        )}
-        <AddPlayerForm addPlayer={this.handleAddPlayer}/>
+        ))}
+        <AddPlayerForm addPlayer={this.handleAddPlayer} />
       </div>
     );
   }
