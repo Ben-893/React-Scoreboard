eb638be295dc80199db584c8b0c68193ef578f02
Benjamin Luke Smith-Gordon <ben.smith.gordon@gmail.com>
2020-08-25 19:30:38 +0100

    Removed logo from Manifest and Removed Static Players from State

--- src/App.js
+++ src/App.js
@@ -5,28 +5,7 @@ import AddPlayerForm from "./AddPlayerForm";
 
 class App extends Component {
   state = {
-    players: [
-      {
-        name: "Guil",
-        score: 0,
-        id: 1,
-      },
-      {
-        name: "Treasure",
-        score: 0,
-        id: 2,
-      },
-      {
-        name: "Ashley",
-        score: 0,
-        id: 3,
-      },
-      {
-        name: "James",
-        score: 0,
-        id: 4,
-      },
-    ],
+    players: [],
   };
 
   prevPlayerId = 4;
@@ -63,7 +42,7 @@ class App extends Component {
   render() {
     return (
       <div className="scoreboard">
-        <Header title="Scoreboard" players={this.state.players} />
+        <Header players={this.state.players} />
 
         {/* Players list */}
         {this.state.players.map((player, index) => (
--- src/Player.js
+++ src/Player.js
@@ -1,7 +1,18 @@
 import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
 import Counter from './Counter';
 
 class Player extends PureComponent {
+
+  static propTypes = {
+    changeScore: PropTypes.func,
+    removePlayer: PropTypes.func,
+    name: PropTypes.string,
+    score: PropTypes.number,
+    id: PropTypes.number,
+    index: PropTypes.number
+  };
+
   render() {
     const {
       name, 
@@ -16,7 +27,7 @@ class Player extends PureComponent {
       <div className="player">
         <span className="player-name">
           <button className="remove-player" onClick={() => removePlayer(id)}>✖</button>
-          { name }
+          {name}
         </span>
   
         <Counter 
